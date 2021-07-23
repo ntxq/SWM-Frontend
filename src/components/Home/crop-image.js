44459e52a930eb701f0ff27cc685210df2acbfe5
ntxq <bkwak@ust.hk>
2021-07-23 20:53:44 +0900

    Magnify preview image on click

--- src/components/Home/crop-image.js
+++ src/components/Home/crop-image.js
@@ -1,10 +1,31 @@
-import React from "react";
-import { Image } from "antd";
+import React, { useState } from "react";
+import { Image, Modal } from "antd";
 
 function CropImage(properties) {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => setIsModalVisible(true);
+  const closeModal = () => setIsModalVisible(false);
+
   return (
     <div className="crop">
-      <Image src={properties.url} className="preview_image" />
+      <Image
+        src={properties.url}
+        onClick={showModal}
+        preview={{
+          visible: false,
+        }}
+        className="preview_image"
+      />
+      <Modal
+        visible={isModalVisible}
+        // eslint-disable-next-line unicorn/no-null
+        footer={null}
+        closable={false}
+        onCancel={closeModal}
+      >
+        <Image src={properties.url} preview={false} />
+      </Modal>
     </div>
   );
 }
