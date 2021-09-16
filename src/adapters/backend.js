9884cac53d904897cc100b9b6dc554f95fd4b8bf
ntxq <bkwak@ust.hk>
2021-09-16 16:13:07 +0800

    Fetch objecturl to upload file

--- src/adapters/backend.js
+++ src/adapters/backend.js
@@ -20,8 +20,6 @@ export async function createProject(imageSlice, title) {
     .post("/upload/segmentation/project", data)
     .then((response) => response.data.request_array);
 
-  const imageMap = {};
-
   for (const reqeustInfo of request_array) {
     await axios.put(reqeustInfo.s3_url, imageSlice.original);
 
@@ -38,14 +36,39 @@ export async function createProject(imageSlice, title) {
       });
   }
 
+  const imageMap = {};
   for (const image of imageSlice) {
-    if (!image.inpaint) continue;
-
-    const blankURL = request_array.find(
+    const { req_id, s3_url, s3_blank_url } = request_array.find(
       (info) => info.filename === image.filename
-    ).s3_blank_url;
+    );
 
-    await axios.put(blankURL, image.inpaint);
+    const originalFile = await fetch(image.original)
+      .then((result) => result.blob())
+      .then((blob) => new File([blob], image.filename, { type: blob.type }));
+
+    await axios.put(s3_url, originalFile);
+    await backendInstance
+      .post("/upload/segmentation/source", {
+        req_id,
+      })
+      .then((response) => response.data.cut_count)
+      .then((cut_count) => {
+        imageMap[image.filename] = {
+          req_id,
+          cut_count,
+        };
+      });
+
+    if (image.inpaint) {
+      const blankFile = await fetch(image.inpaint)
+        .then((result) => result.blob())
+        .then((blob) => new File([blob], image.filename, { type: blob.type }));
+
+      await axios.put(s3_blank_url, blankFile);
+      await backendInstance.post("/upload/segmentation/blank", {
+        req_id,
+      });
+    }
   }
 
   return imageMap;
